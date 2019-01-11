26c9de735118ea86e13ed4571dd493161b7277ec
Chris Gibbons <chris@gbbns.co>
2019-01-11 09:34:37 +0000

    chore(tidy): remove comments

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -32,10 +32,6 @@ const Tags = ({ pageContext, data }) => {
 							)
 						})}
 					</ul>
-					{/*
-									This links to a page that does not yet exist.
-									We'll come back to it!
-								*/}
 					<Link to="/tags">All tags</Link>
 				</div>
 			</section>
