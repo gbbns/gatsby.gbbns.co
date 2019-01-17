2f8c306b1f90eddd892016f57195aebfd52fb548
Chris Gibbons <chris@gbbns.co>
2019-01-17 10:10:15 +0000

    feature(styling): adds in better navigation styling

--- src/components/header.js
+++ src/components/header.js
@@ -55,19 +55,25 @@ const Header = () => (
 					</ul>
 				</nav>
 
-				<aside>
-					<h3>Colophon</h3>
-					<ul>
-						<li>Built using: Visual Studio Code</li>
-						<li>Generated with: gatsby</li>
-						<li>Hosted on: Github</li>
-						<li>Published on: Netlify</li>
-						<li>Background colour: #000000 </li>
-						<li>Typeface: Space Mono</li>
-						<li>H1 colour: #272727</li>
-						<li>H2 colour: #FFFFFF</li>
-						<li>Bodycopy colour: #A3A3A3</li>
-						<li>Background image: On / Off</li>
+				<aside className="c-colophon">
+					<h3>
+						<Link to="/colophon"
+									className="c-colophon__link"
+									activeClassName="is-active">
+							Colophon
+						</Link>
+					</h3>
+					<ul className="c-colophon__list">
+						<li className="c-colophon__items">Built using: Visual Studio Code</li>
+						<li className="c-colophon__items">Generated with: gatsby</li>
+						<li className="c-colophon__items">Hosted on: Github</li>
+						<li className="c-colophon__items">Published on: Netlify</li>
+						<li className="c-colophon__items">Background colour: #000000 </li>
+						<li className="c-colophon__items">Typeface: Space Mono</li>
+						<li className="c-colophon__items">H1 colour: #272727</li>
+						<li className="c-colophon__items">H2 colour: #FFFFFF</li>
+						<li className="c-colophon__items">Bodycopy colour: #A3A3A3</li>
+						<li className="c-colophon__items">Background image: On / Off</li>
 					</ul>
 				</aside>
 
