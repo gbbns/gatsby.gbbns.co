f0205ed168086b6426412e845231c792318a466a
Chris Gibbons <chris@gbbns.co>
2019-01-17 12:18:46 +0000

    chore(content): updated colophon to reflect the website journey

--- src/pages/colophon.js
+++ src/pages/colophon.js
@@ -8,19 +8,22 @@ import '../styles/main.scss'
 export default function Colophon({ data }) {
 	return (
 		<Layout>
-			<SEO title="gbbns.co colophon" keywords={['gatsby', 'application', 'react']} />
+			<SEO title="A colophon for gbbns.co" keywords={['gatsby', 'application', 'react']} />
 
 			<section className="c-content">
 				<div className="c-content__title">
-					<h1>Site colophon</h1>
+					<h1>A colophon for gbbns.co</h1>
 				</div>
 				<div className="c-content__body">
-					<p>This site ~~is built using [Atom](https://atom.io), generated with [Jekyll](https://jekyllrb.com/) and hosted on [GitHub Pages](https://pages.github.com).~~ is built using [Visual Studio Code](https://code.visualstudio.com/), now generated with [Hugo](https://gohugo.io/), hosted on [GitHub](https://github.com/gbbns/gbbns.co) and published on [Netlify](https://www.netlify.com/), phew.</p>
-
-					<p>Currently all pages are stripped right back to the bare bones -- and if they aren't they will be soon -- to enable me to get to grips with Hugo.</p>
-
-					<p>[I plan on plugging in Sass, a task runner of some kind, some PostCSS goodness and at some point, a design](https://github.com/gbbns/gbbns.co/issues).</p>
-
+					<p>Well, this is awkward.</p>
+					<p>This is currently the third iteration of this site, every version has served a purpose, and served it very well.</p>
+					<ul>
+						<li>v1. was developed using <a href="https://atom.io" rel="noopener">Atom</a>, generated with <a href="https://jekyllrb.com/" rel="noopener">Jekyll</a>.</li>
+						<li>v2. generated with <a href="https://gohugo.io/" rel="noopener">Hugo</a>.</li>
+						<li>v3. is now being generated with <a href="https://www.gatsbyjs.org/" rel="noopener">the pretty awesome Gatsby</a></li>
+					</ul>
+					<p>The site is developed using <a href="https://code.visualstudio.com/" rel="noopener">Visual Studio Code</a>.</p>
+					<p>The site is hosted on <a href="https://github.com/gbbns/gbbns.co" rel="noopener">GitHub</a> and published to <a href="https://www.netlify.com/" rel="noopener">Netlify</a></p>
 					<p>I will keep this page updated as I make more changes to the site.</p>
 				</div>
 			</section>
