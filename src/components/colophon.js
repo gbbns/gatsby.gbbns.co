b74c6a9f355850b45fab4ea9d19a93ca312aa226
Chris Gibbons <chris@gbbns.co>
2019-01-17 12:53:43 +0000

    feature(component): break out colophon into it's own component

---
+++ src/components/colophon.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export const Colophon = () => {
+	return (<aside className="c-colophon">
+		<h3>
+			<Link to="/colophon" className="c-colophon__link" activeClassName="is-active">
+				Colophon
+			</Link>
+		</h3>
+		<ul className="c-colophon__list">
+			<li className="c-colophon__items">Built using: Visual Studio Code</li>
+			<li className="c-colophon__items">Generated with: gatsby</li>
+			<li className="c-colophon__items">Hosted on: Github</li>
+			<li className="c-colophon__items">Published on: Netlify</li>
+			<li className="c-colophon__items">Background colour: #000000 </li>
+			<li className="c-colophon__items">Typeface: Space Mono</li>
+			<li className="c-colophon__items">H1 colour: #272727</li>
+			<li className="c-colophon__items">H2 colour: #FFFFFF</li>
+			<li className="c-colophon__items">Bodycopy colour: #A3A3A3</li>
+		</ul>
+	</aside>);
+}
--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../components/header'
+import { Colophon } from '../components/colophon';
 
 const Layout = ({ children}) => {
 	return (
 		<main className="o-content">
 			<Header />
+			<Colophon />
 			<section className="o-content__body">
 				{children}
 			</section>
