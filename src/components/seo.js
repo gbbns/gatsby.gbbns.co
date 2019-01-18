8d3cacc8b35a1636d437aac9f8ed5cf8cf540b5e
Chris Gibbons <chris@gbbns.co>
2019-01-18 16:07:17 +0000

    feat(meta): adds my name to description

--- src/components/seo.js
+++ src/components/seo.js
@@ -8,8 +8,7 @@ function SEO({ description, lang, meta, keywords, title }) {
     <StaticQuery
       query={detailsQuery}
       render={data => {
-        const metaDescription =
-          description || data.site.siteMetadata.siteDescription
+				const metaDescription = data.site.siteMetadata.author + " | " + (description || data.site.siteMetadata.siteDescription);
         return (
           <Helmet
             htmlAttributes={{
