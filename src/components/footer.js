b81ed93644d7e89423ffeba3fcfaa523d4eb67ae
Chris Gibbons <chris@gbbns.co>
2019-01-18 16:06:54 +0000

    chore(lint): class -> className

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
 	const copyrightYear = new Date().getFullYear();
 	return (
 		<footer class="c-footer">
-			<div class="card card--message">
+			<div className="card card--message">
 				<h4>As you've probably spotted I'm designing &amp; building this site "live" and iteratively.</h4>
 				<p>Im the true sprit of Agile, I believe that coding in the open promotes sharing, enables better and faster feedback and provides a great opportunity for learning.</p>
 				<p>Because of this things will inevitably change and possibly break but that's more than ok, you can <a href="mailto:chris@gbbns.co" rel="noopener">email me to let me know</a> if you spot anything.</p>
@@ -14,36 +14,36 @@ export const Footer = () => {
 			<p><a href="mailto:chris@gbbns.co" rel="noopener">chris@gbbns.co</a></p>
 
 			<nav>
-				<ul class="h-list">
-					<li class="h-list__item">
+				<ul className="h-list">
+					<li className="h-list__item">
 						<a href="/colophon">Colophon</a>
 					</li>
-					<li class="h-list__item">
+					<li className="h-list__item">
 						<a href="/now">Now</a>
 					</li>
-					<li class="h-list__item">
+					<li className="h-list__item">
 						<a href="/uses">Uses</a>
 					</li>
-					<li class="h-list__item">
+					<li className="h-list__item">
 						<a href="/tags">Tags</a>
 					</li>
 				</ul>
 			</nav>
 
-			<ul class="h-list ">
-				<li class="h-list__item">
+			<ul className="h-list ">
+				<li className="h-list__item">
 					<a href="https://github.com/gbbns" rel="noopener">GitHub</a>
 				</li>
-				<li class="h-list__item">
+				<li className="h-list__item">
 					<a href="https://www.instagram.com/_gbbns" rel="noopener">Instagram</a>
 				</li>
-				<li class="h-list__item">
+				<li className="h-list__item">
 					<a href="https://twitter.com/_gbbns" rel="noopener">Twitter</a>
 				</li>
-				<li class="h-list__item">
+				<li className="h-list__item">
 					<a href="https://uk.linkedin.com/in/gbbns" rel="noopener">LinkedIn</a>
 				</li>
-				<li class="h-list__item">
+				<li className="h-list__item">
 					<a href="https://500px.com/gbbns" rel="noopener">500px</a>
 				</li>
 			</ul>
